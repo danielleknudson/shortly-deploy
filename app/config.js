d80cc888185b3c6ecbd69cc1c59f50ad838833a3
Danielle Knudson <knudson.danielle@gmail.com>
2015-06-11 17:23:47 -0700

    Use production host for mongoose connection if in production environment.
    Start refactor.

--- app/config.js
+++ app/config.js
@@ -1,20 +1,21 @@
-var Mongoose = require('mongoose');
-var Schema = Mongoose.Schema;
-var path = require('path');
-var crypto = require('crypto');
-var bcrypt = require('bcrypt-nodejs');
-var Promise = require('bluebird');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
-Mongoose.connect('mongodb://127.0.0.1/shortliestdb');
+// Use production URI if in production environment, 
+// else use localhost
+var mongoURI = process.env.MONGOLAB_URI || 'mongodb://127.0.0.1/bittydb'
+mongoose.connect(mongoURI);
 
-var db = Mongoose.connection;
+var db = mongoose.connection;
 
-db.on('error', function(err){
-  console.log('connection error:', err);
-});
+db.on('error', console.error.bind(console, 'connection error: '));
+
 db.once('open', function(){
   console.log('database connection established');
 });
+
+module.exports = db;
+
 
 // db schema urls
 db.urlsSchema = new Schema({
@@ -70,48 +71,3 @@ db.users_urlsSchema = new Schema({
 });
 
 
-// var Bookshelf = require('bookshelf');
-// var path = require('path');
-
-// var db = Bookshelf.initialize({
-//   client: 'sqlite3',
-//   connection: {
-//     host: '127.0.0.1',
-//     user: 'your_database_user',
-//     password: 'password',
-//     database: 'shortlydb',
-//     charset: 'utf8',
-//     filename: path.join(__dirname, '../db/shortly.sqlite')
-//   }
-// });
-
-// db.knex.schema.hasTable('urls').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('urls', function (link) {
-//       link.increments('id').primary();
-//       link.string('url', 255);
-//       link.string('base_url', 255);
-//       link.string('code', 100);
-//       link.string('title', 255);
-//       link.integer('visits');
-//       link.timestamps();
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
-
-// db.knex.schema.hasTable('users').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('users', function (user) {
-//       user.increments('id').primary();
-//       user.string('username', 100).unique();
-//       user.string('password', 100);
-//       user.timestamps();
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
-
-module.exports = db;
