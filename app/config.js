4140ea223648e69291eda8787c9c78d2c43e6b03
Danielle Knudson <knudson.danielle@gmail.com>
2015-06-11 19:06:02 -0700

    Fix a few bugs leftover from refactoring code to support mongoose

--- app/config.js
+++ app/config.js
@@ -3,9 +3,9 @@ var Schema = mongoose.Schema;
 
 // Use production URI if in production environment, 
 // else use localhost
-var mongoURI = process.env.MONGOLAB_URI || 'mongodb://127.0.0.1/bittydb'
+var mongoURI = process.env.MONGOLAB_URI || 'mongodb://localhost/bittydb';
 mongoose.connect(mongoURI);
-
+console.log(process.env.MONGOLAB_URI)
 var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error: '));
