e730bd6f76f9df749c791a660dbe0a1874d1563e
Danielle Knudson <knudson.danielle@gmail.com>
2015-06-11 17:27:15 -0700

    Begin refactor of config.js.
    Clean up config.js mongoose connection code.
    Move user and link schema declaration to corresponding files in models/.

--- app/models/link.js
+++ app/models/link.js
@@ -1,21 +1,27 @@
 var db = require('../config');
 var crypto = require('crypto');
 
-var Link = db.model('Link', db.urlsSchema);
+// db schema urls
+db.urlsSchema = new Schema({
+  url: String,
+  base_url: String,
+  code: String,
+  title: String,
+  visits: {type: Number, default: 0},
+  timestamps: {
+    createdAt: {type: Date, default: Date.now},
+    modifiedAt: {type: Date, default: Date.now}
+  }
+});
 
-// var Link = db.Model.extend({
-//   tableName: 'urls',
-//   hasTimestamps: true,
-//   defaults: {
-//     visits: 0
-//   },
-  // initialize: function(){
-  //   this.on('creating', function(model, attrs, options){
-  //     var shasum = crypto.createHash('sha1');
-  //     shasum.update(model.get('url'));
-  //     model.set('code', shasum.digest('hex').slice(0, 5));
-  //   });
-  // }
-// });
+db.urlsSchema.pre('save', function(next){
+  var url = this;
+  var shasum = crypto.createHash('sha1');
+  shasum.update(url.url);
+  url.code = shasum.digest('hex').slice(0, 5);
+  next();
+});
+
+var Link = db.model('Link', db.urlsSchema);
 
 module.exports = Link;
--- app/models/user.js
+++ app/models/user.js
@@ -2,27 +2,32 @@ var db = require('../config');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 
+// db schema users
+db.usersSchema = new Schema({
+  username: String,
+  password: String,
+  timestamps: {
+    createdAt: {type: Date, default: Date.now},
+    modifiedAt: {type: Date, default: Date.now}
+  }
+});
+
+db.usersSchema.pre('save', function(next){
+  var user = this;
+  var cipher = Promise.promisify(bcrypt.hash);
+  return cipher(user.password, null, null).bind(user)
+    .then(function(hash) {
+      user.password = hash;
+    });
+  next();
+});
+
+db.usersSchema.methods.comparePassword = function(attemptedPassword, callback) {
+    bcrypt.compare(attemptedPassword, this.password, function(err, isMatch) {
+      callback(isMatch);
+    });
+};
+
 var User = db.model('User', db.usersSchema);
-
-
-// var User = db.Model.extend({
-//   tableName: 'users',
-//   hasTimestamps: true,
-//   initialize: function(){
-//     this.on('creating', this.hashPassword);
-//   },
-//   comparePassword: function(attemptedPassword, callback) {
-//     bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-//       callback(isMatch);
-//     });
-//   },
-//   hashPassword: function(){
-//     var cipher = Promise.promisify(bcrypt.hash);
-//     return cipher(this.get('password'), null, null).bind(this)
-//       .then(function(hash) {
-//         this.set('password', hash);
-//       });
-//   }
-// });
 
 module.exports = User;
